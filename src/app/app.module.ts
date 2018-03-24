c149fc01be2882d3d7bd1695431f53fcf269f50a
David Poulis <david_fci@outlook.com>
2018-03-24 16:48:49 +0200

    Getting Started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,19 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { AboutPage } from '../pages/about/about';
+import { MenuPage } from '../pages/menu/menu';
+import { ContactPage } from '../pages/contact/contact'; 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage,
-    ListPage
+    HomePage,   
+    AboutPage,
+    MenuPage,
+    ContactPage
   ],
   imports: [
     BrowserModule,
@@ -23,7 +26,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    AboutPage,
+    MenuPage,
+    ContactPage
   ],
   providers: [
     StatusBar,
